981f55c91b740f8e2222f9a3ed6c4c74ff0036ac
ORLANDO RUBIO RIVAS <orlando.rubio@HELISA.LOCAL>
2018-10-01 13:53:54 -0500

    base de proyecto

---
+++ carro/src/main/java/com/example/carro/CarroApplication.java
@@ -0,0 +1,12 @@
+package com.example.carro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarroApplication.class, args);
+	}
+}
