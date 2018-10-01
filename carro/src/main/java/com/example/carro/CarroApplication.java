82258a24aadd5accc9e177c63e667681ae3301a5
ORLANDO RUBIO RIVAS <orlando.rubio@HELISA.LOCAL>
2018-10-01 15:16:42 -0500

    conexion a BD, habilitar auditoria jpa, clase auditModel que contiene caracteristicas o propiedades que sirven para heredar en las otras clases

--- carro/src/main/java/com/example/carro/CarroApplication.java
+++ carro/src/main/java/com/example/carro/CarroApplication.java
@@ -2,8 +2,10 @@ package com.example.carro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CarroApplication {
 
 	public static void main(String[] args) {
